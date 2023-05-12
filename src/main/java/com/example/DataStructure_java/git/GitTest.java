7163bff1fbee702a2f8c9601be450b19ce2ed91b
qiyang <qiyang@126.com>
2023-05-12 11:22:08 +0800

    hot-fix commit2

---
+++ src/main/java/com/example/DataStructure_java/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.example.DataStructure_java.git;
+/**
+ * @author 齐阳
+ * @version 1.0
+ */
+@SuppressWarnings({"all"})
+public class GitTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+    }
+}
